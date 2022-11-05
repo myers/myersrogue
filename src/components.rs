648b037e574a99908204b958431e417c828a5bbb
Myers Carpenter <myers@maski.org>
2022-11-05 13:13:36 -0400

    health

--- src/components.rs
+++ src/components.rs
@@ -20,3 +20,12 @@ pub struct WantsToMove {
     pub entity: Entity,
     pub destination: Point,
 }
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Health {
+    pub current: i32,
+    pub max: i32,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Name(pub String);
