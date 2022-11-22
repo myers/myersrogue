cd89d60d2a3ec7a9e3811e26eca4ceb60de91d6e
Myers Carpenter <myers@maski.org>
2022-11-22 17:28:59 -0500

    bevy web

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ mod prelude {
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
-    pub use crate::camera::*;
+    pub use crate::camera::Camera;
     pub use crate::components::*;
     pub use crate::events::*;
     pub use crate::game_stage::*;
